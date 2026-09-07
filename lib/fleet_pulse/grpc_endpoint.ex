5a942f5917719a0331141ae95d345c63b3d3d237
Wildan Frananda <wildanrailfanskrl@gmail.com>
2026-09-07 14:35:04 +0700

    feat: add RequestIdInterceptor for logging correlation IDs in gRPC calls

--- lib/fleet_pulse/grpc_endpoint.ex
+++ lib/fleet_pulse/grpc_endpoint.ex
@@ -4,6 +4,7 @@ defmodule FleetPulse.GrpcEndpoint do
   """
   use GRPC.Endpoint
 
+  intercept FleetPulse.Observability.RequestIdInterceptor
   intercept FleetPulse.Security.PeerAuthorizationInterceptor
 
   run FleetPulse.CourierTelemetryServer
